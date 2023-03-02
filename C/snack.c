54623ba1dd010606f600e0584e662e19735d6203
Kenta Kato <01keny01@gmail.com>
2023-03-02 11:17:23 +0900

    add: solve a question

---
+++ C/snack.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void)
+{
+  long a, b, r, x, tmp;
+  scanf("%ld%ld", &a, &b);
+  x = a * b;
+ 
+  /* 自然数 a > b を確認・入替 */
+  if(a<b){
+    tmp = a;
+    a = b;
+    b = tmp;
+  }
+ 
+  /* ユークリッドの互除法 */
+  r = a % b;
+  while(r!=0){
+    a = b;
+    b = r;
+    r = a % b;
+  }
+ 
+  /* 最小公倍数を出力 */
+  printf("%ld\n", x/b);
+ 
+  return 0;
+
+}
